4a9f39df10c238aa5c2c63077c72cc1f058fb720
Satya Dodda <satya.tejaswi99@gmail.com>
2024-02-03 17:23:12 +0530

    Initial Commit

---
+++ PlaceItOnGit/PlaceItOnGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceItOnGitApp.swift
+//  PlaceItOnGit
+//
+//  Created by C7K6SW on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaceItOnGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
